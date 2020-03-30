83d7d2b713dc8f72dca67c6d7368429002537fe8
Liqing YANG <slyly2@exmail.nottingham.ac.uk>
2020-03-30 13:18:46 +0800

    refactor: CX & OX

--- src/com/aim/project/pwp/heuristics/CX.java
+++ src/com/aim/project/pwp/heuristics/CX.java
@@ -10,86 +10,32 @@ import com.aim.project.pwp.interfaces.ObjectiveFunctionInterface;
 import com.aim.project.pwp.interfaces.PWPSolutionInterface;
 import com.aim.project.pwp.interfaces.XOHeuristicInterface;
 
-public class CX implements XOHeuristicInterface {
+public class CX extends XOHeuristic {
 
-  private final Random oRandom;
+	public CX(Random oRandom) {
+		super(oRandom);
+	}
 
-  private ObjectiveFunctionInterface oObjectiveFunction;
+	@Override
+	protected void crossoverAlgorithm(List<Integer> parent1, List<Integer> parent2, int numberOfDeliveryLocations) {
+		List<Integer> cycleIndices = new ArrayList<>();
 
-  public CX(Random oRandom) {
-    this.oRandom = oRandom;
-  }
+		int index = oRandom.nextInt(numberOfDeliveryLocations);
 
-  @Override
-  public double apply(PWPSolutionInterface solution, double depthOfSearch, double intensityOfMutation) {
-    return solution.getObjectiveFunctionValue();
-  }
+		do {
+			cycleIndices.add(index);
+			int locationInP2 = parent2.get(index);
+			index = parent1.indexOf(locationInP2);
+		} while (index != cycleIndices.get(0));
 
-  @Override
+		for (int cycleIndex : cycleIndices) {
+			swap(parent1, parent2, cycleIndex);
+		}
+	}
 
-	public double apply(PWPSolutionInterface p1, PWPSolutionInterface p2, PWPSolutionInterface c, double depthOfSearch, double intensityOfMutation) {
-    int times = 1 + (int) (intensityOfMutation * 10 / 2);
-
-    int[] deliveryLocationsP1 = p1.getSolutionRepresentation().getSolutionRepresentation();
-    int[] deliveryLocationsP2 = p2.getSolutionRepresentation().getSolutionRepresentation();
-
-    int numberOfDeliveryLocations = c.getSolutionRepresentation().getNumberOfLocations();
-
-    List<Integer> parent1 = Arrays.stream(deliveryLocationsP1).boxed().collect(Collectors.toList());
-    List<Integer> parent2 = Arrays.stream(deliveryLocationsP2).boxed().collect(Collectors.toList());
-
-    while (times > 0) {
-      List<Integer> cycleIndices = new ArrayList<>();
-
-      int index = oRandom.nextInt(numberOfDeliveryLocations);
-
-      do {
-        cycleIndices.add(index);
-        int locationInP2 = parent2.get(index);
-        index = parent1.indexOf(locationInP2);
-      } while (index != cycleIndices.get(0));
-
-      for (int cycleIndex : cycleIndices) {
-				swap(parent1, parent2, cycleIndex);
-			}
-
-      times--;
-    }
-
-    List<Integer> candidate = oRandom.nextDouble() >= 0.5 ? parent1 : parent2;
-
-    int[] newRepresentation = candidate.stream().mapToInt(i -> i).toArray();
-    c.getSolutionRepresentation().setSolutionRepresentation(newRepresentation);
-
-    double functionValue = this.oObjectiveFunction.getObjectiveFunctionValue(c.getSolutionRepresentation());
-    c.setObjectiveFunctionValue(functionValue);
-
-    return c.getObjectiveFunctionValue();
-  }
-
-  private void swap(List<Integer> l1, List<Integer> l2, int index) {
+	private void swap(List<Integer> l1, List<Integer> l2, int index) {
   	Integer temp = l1.get(index);
   	l1.set(index, l2.get(index));
   	l2.set(index, temp);
 	}
-
-  @Override
-  public boolean isCrossover() {
-    return true;
-  }
-
-  @Override
-  public boolean usesIntensityOfMutation() {
-    return true;
-  }
-
-  @Override
-  public boolean usesDepthOfSearch() {
-    return false;
-  }
-
-  @Override
-  public void setObjectiveFunction(ObjectiveFunctionInterface oObjectiveFunction) {
-    this.oObjectiveFunction = oObjectiveFunction;
-  }
 }
--- src/com/aim/project/pwp/heuristics/OX.java
+++ src/com/aim/project/pwp/heuristics/OX.java
@@ -7,96 +7,43 @@ import com.aim.project.pwp.interfaces.ObjectiveFunctionInterface;
 import com.aim.project.pwp.interfaces.PWPSolutionInterface;
 import com.aim.project.pwp.interfaces.XOHeuristicInterface;
 
-public class OX implements XOHeuristicInterface {
-
-	private final Random oRandom;
-
-	private ObjectiveFunctionInterface oObjectiveFunction;
+public class OX extends XOHeuristic {
 
 	public OX(Random oRandom) {
-		this.oRandom = oRandom;
+		super(oRandom);
 	}
 
 	@Override
-	public double apply(PWPSolutionInterface oSolution, double dDepthOfSearch, double dIntensityOfMutation) {
-		return oSolution.getObjectiveFunctionValue();
-	}
+	protected void crossoverAlgorithm(List<Integer> parent1, List<Integer> parent2, int numberOfDeliveryLocations) {
+		// avoid choose the cut points at the start and end simultaneously
+		int number1 = oRandom.nextInt(numberOfDeliveryLocations);
+		int number2 = oRandom.nextInt(numberOfDeliveryLocations + 1);
 
-	@Override
-	public double apply(PWPSolutionInterface p1, PWPSolutionInterface p2, PWPSolutionInterface c, double depthOfSearch, double intensityOfMutation) {
-		int times = 1 + (int) (intensityOfMutation * 10 / 2);
+		int start = Math.min(number1, number2);
+		int end = Math.max(number1, number2);
 
-		int[] deliveryLocationsP1 = p1.getSolutionRepresentation().getSolutionRepresentation();
-		int[] deliveryLocationsP2 = p2.getSolutionRepresentation().getSolutionRepresentation();
+		List<Integer> child1 = new ArrayList<>(parent1.subList(start, end));
+		List<Integer> child2 = new ArrayList<>(parent2.subList(start, end));
 
-		int numberOfDeliveryLocations = c.getSolutionRepresentation().getNumberOfLocations();
+		for (int i = 0; i < numberOfDeliveryLocations; i++) {
+			int currentLocationIndex = (i + end) % numberOfDeliveryLocations;
 
-		List<Integer> parent1 = Arrays.stream(deliveryLocationsP1).boxed().collect(Collectors.toList());
-		List<Integer> parent2 = Arrays.stream(deliveryLocationsP2).boxed().collect(Collectors.toList());
+			int currentLocationInP1 = parent1.get(currentLocationIndex);
+			int currentLocationInP2 = parent2.get(currentLocationIndex);
 
-		while (times > 0) {
-			// avoid choose the cut points at the start and end simultaneously
-			int number1 = oRandom.nextInt(numberOfDeliveryLocations);
-			int number2 = oRandom.nextInt(numberOfDeliveryLocations + 1);
-
-			int start = Math.min(number1, number2);
-			int end = Math.max(number1, number2);
-
-			List<Integer> child1 = new ArrayList<>(parent1.subList(start, end));
-			List<Integer> child2 = new ArrayList<>(parent2.subList(start, end));
-
-			for (int i = 0; i < numberOfDeliveryLocations; i++) {
-				int currentLocationIndex = (i + end) % numberOfDeliveryLocations;
-
-				int currentLocationInP1 = parent1.get(currentLocationIndex);
-				int currentLocationInP2 = parent2.get(currentLocationIndex);
-
-				if (!child1.contains(currentLocationInP2)) {
-					child1.add(currentLocationInP2);
-				}
-
-				if (!child2.contains(currentLocationInP1)) {
-					child2.add(currentLocationInP1);
-				}
+			if (!child1.contains(currentLocationInP2)) {
+				child1.add(currentLocationInP2);
 			}
 
-			Collections.rotate(child1, start);
-			Collections.rotate(child2, start);
-
-			Collections.copy(parent1, child1);
-			Collections.copy(parent2, child2);
-
-			times--;
+			if (!child2.contains(currentLocationInP1)) {
+				child2.add(currentLocationInP1);
+			}
 		}
 
-		List<Integer> candidate = oRandom.nextDouble() >= 0.5 ? parent1 : parent2;
+		Collections.rotate(child1, start);
+		Collections.rotate(child2, start);
 
-		int[] newRepresentation = candidate.stream().mapToInt(i -> i).toArray();
-		c.getSolutionRepresentation().setSolutionRepresentation(newRepresentation);
-
-		double functionValue = this.oObjectiveFunction.getObjectiveFunctionValue(c.getSolutionRepresentation());
-		c.setObjectiveFunctionValue(functionValue);
-
-		return functionValue;
-	}
-
-	@Override
-	public boolean isCrossover() {
-		return true;
-	}
-
-	@Override
-	public boolean usesIntensityOfMutation() {
-		return true;
-	}
-
-	@Override
-	public boolean usesDepthOfSearch() {
-		return false;
-	}
-
-	@Override
-	public void setObjectiveFunction(ObjectiveFunctionInterface f) {
-		this.oObjectiveFunction = f;
+		Collections.copy(parent1, child1);
+		Collections.copy(parent2, child2);
 	}
 }
