f910b14750beef5b1c6ba74b64ef4c0f5e8a8f07
Liqing YANG <slyly2@exmail.nottingham.ac.uk>
2020-04-01 01:26:18 +0800

    style

--- src/com/aim/project/pwp/runners/HH_Runner_Visual.java
+++ src/com/aim/project/pwp/runners/HH_Runner_Visual.java
@@ -1,6 +1,5 @@
 package com.aim.project.pwp.runners;
 
-
 import java.awt.Color;
 
 import com.aim.project.pwp.AIM_PWP;
@@ -17,38 +16,34 @@ import AbstractClasses.HyperHeuristic;
  */
 public abstract class HH_Runner_Visual {
 
-	public HH_Runner_Visual() {
-		
-	}
-	
-	public void run() {
-		
-		long seed = 13032020l;
-		long timeLimit = 1_000l;
-		AIM_PWP problem = new AIM_PWP(seed);
-		problem.loadInstance(0);
-		HyperHeuristic hh = getHyperHeuristic(seed);
-		hh.setTimeLimit(timeLimit);
-		hh.loadProblemDomain(problem);
-		hh.run();
-		
-		System.out.println("f(s_best) = " + hh.getBestSolutionValue());
-		new PWPView(problem.oInstance, problem, Color.RED, Color.GREEN);
-		
-	}
-	
-	/** 
-	 * Transforms the best solution found, represented as a TSPSolution, into an ordering of Location's
-	 * which the visualiser tool uses to draw the tour.
-	 */
-	protected Location[] transformSolution(PWPSolution solution, AIM_PWP problem) {
-		
-		return problem.getRouteOrderedByLocations();
-	}
-	
-	/**
-	 * Allows a general visualiser runner by making the HyperHeuristic abstract.
-	 * You can sub-class this class to run any hyper-heuristic that you want.
-	 */
-	protected abstract HyperHeuristic getHyperHeuristic(long seed);
+  public HH_Runner_Visual() {}
+
+  public void run() {
+
+    long seed = 13032020L;
+    long timeLimit = 60_000L;
+    AIM_PWP problem = new AIM_PWP(seed);
+    problem.loadInstance(4);
+    HyperHeuristic hh = getHyperHeuristic(seed);
+    hh.setTimeLimit(timeLimit);
+    hh.loadProblemDomain(problem);
+    hh.run();
+
+    System.out.println("f(s_best) = " + hh.getBestSolutionValue());
+    new PWPView(problem.oInstance, problem, Color.RED, Color.GREEN);
+  }
+
+  /**
+   * Transforms the best solution found, represented as a TSPSolution, into an ordering of
+   * Location's which the visualiser tool uses to draw the tour.
+   */
+  protected Location[] transformSolution(PWPSolution solution, AIM_PWP problem) {
+    return problem.getRouteOrderedByLocations();
+  }
+
+  /**
+   * Allows a general visualiser runner by making the HyperHeuristic abstract. You can sub-class
+   * this class to run any hyper-heuristic that you want.
+   */
+  protected abstract HyperHeuristic getHyperHeuristic(long seed);
 }
